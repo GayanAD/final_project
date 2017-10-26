2c3f7900121c551242740c4f993064b2ffeeb45f
TharuKarunarathne <tnilakshikarunarathne96@gmail.com>
2017-10-26 07:11:49 -0700

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace MetroDashBoard
+﻿namespace WindowsFormsApplication5
 {
-    partial class Dashboard
+    partial class Form1
     {
         /// <summary>
         /// Required designer variable.
@@ -28,215 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.metroTile3 = new MetroFramework.Controls.MetroTile();
-            this.metroTile4 = new MetroFramework.Controls.MetroTile();
-            this.metroTile2 = new MetroFramework.Controls.MetroTile();
-            this.metroTile1 = new MetroFramework.Controls.MetroTile();
-            this.metroTile5 = new MetroFramework.Controls.MetroTile();
-            this.metroTile6 = new MetroFramework.Controls.MetroTile();
-            this.metroTile7 = new MetroFramework.Controls.MetroTile();
-            this.metroTile8 = new MetroFramework.Controls.MetroTile();
-            this.metroTile9 = new MetroFramework.Controls.MetroTile();
+            this.components = new System.ComponentModel.Container();
+            this.Mpanel = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.Mpanel.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // metroTile3
+            // Mpanel
             // 
-            this.metroTile3.ActiveControl = null;
-            this.metroTile3.BackColor = System.Drawing.Color.Gray;
-            this.metroTile3.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.metroTile3.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.metroTile3.Location = new System.Drawing.Point(559, 110);
-            this.metroTile3.Margin = new System.Windows.Forms.Padding(4);
-            this.metroTile3.Name = "metroTile3";
-            this.metroTile3.Size = new System.Drawing.Size(178, 120);
-            this.metroTile3.Style = MetroFramework.MetroColorStyle.Teal;
-            this.metroTile3.TabIndex = 2;
-            this.metroTile3.Text = "Accounts";
-            this.metroTile3.TileTextFontSize = MetroFramework.MetroTileTextSize.Tall;
-            this.metroTile3.TileTextFontWeight = MetroFramework.MetroTileTextWeight.Bold;
-            this.metroTile3.UseSelectable = true;
+            this.Mpanel.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.Mpanel.Controls.Add(this.button7);
+            this.Mpanel.Controls.Add(this.button6);
+            this.Mpanel.Controls.Add(this.button5);
+            this.Mpanel.Controls.Add(this.button4);
+            this.Mpanel.Controls.Add(this.button3);
+            this.Mpanel.Controls.Add(this.button2);
+            this.Mpanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.Mpanel.Location = new System.Drawing.Point(40, 0);
+            this.Mpanel.Name = "Mpanel";
+            this.Mpanel.Size = new System.Drawing.Size(129, 283);
+            this.Mpanel.TabIndex = 1;
             // 
-            // metroTile4
+            // button4
             // 
-            this.metroTile4.ActiveControl = null;
-            this.metroTile4.BackColor = System.Drawing.Color.Maroon;
-            this.metroTile4.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.metroTile4.ForeColor = System.Drawing.Color.Maroon;
-            this.metroTile4.Location = new System.Drawing.Point(51, 250);
-            this.metroTile4.Margin = new System.Windows.Forms.Padding(4);
-            this.metroTile4.Name = "metroTile4";
-            this.metroTile4.Size = new System.Drawing.Size(182, 132);
-            this.metroTile4.Style = MetroFramework.MetroColorStyle.Red;
-            this.metroTile4.TabIndex = 3;
-            this.metroTile4.Text = "Stocks";
-            this.metroTile4.TileTextFontSize = MetroFramework.MetroTileTextSize.Tall;
-            this.metroTile4.TileTextFontWeight = MetroFramework.MetroTileTextWeight.Bold;
-            this.metroTile4.UseSelectable = true;
+            this.button4.Location = new System.Drawing.Point(3, 97);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(123, 42);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Stocks";
+            this.button4.UseVisualStyleBackColor = true;
             // 
-            // metroTile2
+            // button3
             // 
-            this.metroTile2.ActiveControl = null;
-            this.metroTile2.BackColor = System.Drawing.Color.Gray;
-            this.metroTile2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.metroTile2.ForeColor = System.Drawing.Color.Gray;
-            this.metroTile2.Location = new System.Drawing.Point(241, 250);
-            this.metroTile2.Margin = new System.Windows.Forms.Padding(4);
-            this.metroTile2.Name = "metroTile2";
-            this.metroTile2.Size = new System.Drawing.Size(188, 132);
-            this.metroTile2.Style = MetroFramework.MetroColorStyle.Black;
-            this.metroTile2.TabIndex = 1;
-            this.metroTile2.Text = "Orders";
-            this.metroTile2.Theme = MetroFramework.MetroThemeStyle.Dark;
-            this.metroTile2.TileTextFontSize = MetroFramework.MetroTileTextSize.Tall;
-            this.metroTile2.TileTextFontWeight = MetroFramework.MetroTileTextWeight.Bold;
-            this.metroTile2.UseSelectable = true;
-            this.metroTile2.UseTileImage = true;
+            this.button3.Location = new System.Drawing.Point(3, 51);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(123, 40);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Order";
+            this.button3.UseVisualStyleBackColor = true;
             // 
-            // metroTile1
+            // button2
             // 
-            this.metroTile1.ActiveControl = null;
-            this.metroTile1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.metroTile1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.metroTile1.ForeColor = System.Drawing.SystemColors.ControlDark;
-            this.metroTile1.Location = new System.Drawing.Point(51, 110);
-            this.metroTile1.Margin = new System.Windows.Forms.Padding(4);
-            this.metroTile1.Name = "metroTile1";
-            this.metroTile1.Size = new System.Drawing.Size(378, 132);
-            this.metroTile1.Style = MetroFramework.MetroColorStyle.Green;
-            this.metroTile1.TabIndex = 0;
-            this.metroTile1.Text = "Registation";
-            this.metroTile1.Theme = MetroFramework.MetroThemeStyle.Dark;
-            this.metroTile1.TileImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.metroTile1.TileTextFontSize = MetroFramework.MetroTileTextSize.Tall;
-            this.metroTile1.TileTextFontWeight = MetroFramework.MetroTileTextWeight.Bold;
-            this.metroTile1.UseSelectable = true;
-            this.metroTile1.UseTileImage = true;
-            this.metroTile1.Click += new System.EventHandler(this.metroTile1_Click);
+            this.button2.Location = new System.Drawing.Point(3, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 42);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Registation";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // metroTile5
+            // timer1
             // 
-            this.metroTile5.ActiveControl = null;
-            this.metroTile5.BackColor = System.Drawing.Color.Gray;
-            this.metroTile5.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.metroTile5.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.metroTile5.Location = new System.Drawing.Point(559, 238);
-            this.metroTile5.Margin = new System.Windows.Forms.Padding(4);
-            this.metroTile5.Name = "metroTile5";
-            this.metroTile5.Size = new System.Drawing.Size(178, 132);
-            this.metroTile5.Style = MetroFramework.MetroColorStyle.Silver;
-            this.metroTile5.TabIndex = 4;
-            this.metroTile5.Text = "Reports";
-            this.metroTile5.TileTextFontSize = MetroFramework.MetroTileTextSize.Tall;
-            this.metroTile5.TileTextFontWeight = MetroFramework.MetroTileTextWeight.Bold;
-            this.metroTile5.UseSelectable = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // metroTile6
+            // button1
             // 
-            this.metroTile6.ActiveControl = null;
-            this.metroTile6.BackColor = System.Drawing.Color.Maroon;
-            this.metroTile6.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.metroTile6.ForeColor = System.Drawing.Color.Maroon;
-            this.metroTile6.Location = new System.Drawing.Point(636, 398);
-            this.metroTile6.Margin = new System.Windows.Forms.Padding(4);
-            this.metroTile6.Name = "metroTile6";
-            this.metroTile6.Size = new System.Drawing.Size(182, 132);
-            this.metroTile6.Style = MetroFramework.MetroColorStyle.Purple;
-            this.metroTile6.TabIndex = 5;
-            this.metroTile6.Text = "Help";
-            this.metroTile6.TileTextFontSize = MetroFramework.MetroTileTextSize.Tall;
-            this.metroTile6.TileTextFontWeight = MetroFramework.MetroTileTextWeight.Bold;
-            this.metroTile6.UseSelectable = true;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(40, 280);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "=";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // metroTile7
+            // panel1
             // 
-            this.metroTile7.ActiveControl = null;
-            this.metroTile7.BackColor = System.Drawing.Color.Maroon;
-            this.metroTile7.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.metroTile7.ForeColor = System.Drawing.Color.Maroon;
-            this.metroTile7.Location = new System.Drawing.Point(441, 398);
-            this.metroTile7.Margin = new System.Windows.Forms.Padding(4);
-            this.metroTile7.Name = "metroTile7";
-            this.metroTile7.Size = new System.Drawing.Size(182, 132);
-            this.metroTile7.Style = MetroFramework.MetroColorStyle.Magenta;
-            this.metroTile7.TabIndex = 6;
-            this.metroTile7.Text = "About";
-            this.metroTile7.TileTextFontSize = MetroFramework.MetroTileTextSize.Tall;
-            this.metroTile7.TileTextFontWeight = MetroFramework.MetroTileTextWeight.Bold;
-            this.metroTile7.UseSelectable = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(40, 283);
+            this.panel1.TabIndex = 0;
             // 
-            // metroTile8
+            // button5
             // 
-            this.metroTile8.ActiveControl = null;
-            this.metroTile8.BackColor = System.Drawing.Color.Maroon;
-            this.metroTile8.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.metroTile8.ForeColor = System.Drawing.Color.Maroon;
-            this.metroTile8.Location = new System.Drawing.Point(246, 398);
-            this.metroTile8.Margin = new System.Windows.Forms.Padding(4);
-            this.metroTile8.Name = "metroTile8";
-            this.metroTile8.Size = new System.Drawing.Size(182, 132);
-            this.metroTile8.Style = MetroFramework.MetroColorStyle.Lime;
-            this.metroTile8.TabIndex = 7;
-            this.metroTile8.Text = "Settings";
-            this.metroTile8.TileTextFontSize = MetroFramework.MetroTileTextSize.Tall;
-            this.metroTile8.TileTextFontWeight = MetroFramework.MetroTileTextWeight.Bold;
-            this.metroTile8.UseSelectable = true;
+            this.button5.Location = new System.Drawing.Point(3, 145);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(123, 33);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "Tool";
+            this.button5.UseVisualStyleBackColor = true;
             // 
-            // metroTile9
+            // button6
             // 
-            this.metroTile9.ActiveControl = null;
-            this.metroTile9.BackColor = System.Drawing.Color.Maroon;
-            this.metroTile9.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.metroTile9.ForeColor = System.Drawing.Color.Maroon;
-            this.metroTile9.Location = new System.Drawing.Point(51, 398);
-            this.metroTile9.Margin = new System.Windows.Forms.Padding(4);
-            this.metroTile9.Name = "metroTile9";
-            this.metroTile9.Size = new System.Drawing.Size(182, 132);
-            this.metroTile9.Style = MetroFramework.MetroColorStyle.Blue;
-            this.metroTile9.TabIndex = 8;
-            this.metroTile9.Text = "Tools";
-            this.metroTile9.TileTextFontSize = MetroFramework.MetroTileTextSize.Tall;
-            this.metroTile9.TileTextFontWeight = MetroFramework.MetroTileTextWeight.Bold;
-            this.metroTile9.UseSelectable = true;
+            this.button6.Location = new System.Drawing.Point(3, 184);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(123, 35);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "Reports";
+            this.button6.UseVisualStyleBackColor = true;
             // 
-            // Dashboard
+            // button7
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.button7.Location = new System.Drawing.Point(3, 225);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(123, 40);
+            this.button7.TabIndex = 5;
+            this.button7.Text = "Settings";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BorderStyle = MetroFramework.Forms.MetroFormBorderStyle.FixedSingle;
-            this.ClientSize = new System.Drawing.Size(846, 557);
-            this.Controls.Add(this.metroTile9);
-            this.Controls.Add(this.metroTile8);
-            this.Controls.Add(this.metroTile7);
-            this.Controls.Add(this.metroTile6);
-            this.Controls.Add(this.metroTile5);
-            this.Controls.Add(this.metroTile4);
-            this.Controls.Add(this.metroTile3);
-            this.Controls.Add(this.metroTile2);
-            this.Controls.Add(this.metroTile1);
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ForeColor = System.Drawing.Color.SlateGray;
-            this.Margin = new System.Windows.Forms.Padding(4);
-            this.Name = "Dashboard";
-            this.Padding = new System.Windows.Forms.Padding(27, 69, 27, 23);
-            this.Style = MetroFramework.MetroColorStyle.Lime;
-            this.Text = "Dashboard";
-            this.TransparencyKey = System.Drawing.Color.Lime;
-            this.Load += new System.EventHandler(this.Dashboard_Load);
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(478, 283);
+            this.Controls.Add(this.Mpanel);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Mpanel.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private MetroFramework.Controls.MetroTile metroTile1;
-        private MetroFramework.Controls.MetroTile metroTile2;
-        private MetroFramework.Controls.MetroTile metroTile3;
-        private MetroFramework.Controls.MetroTile metroTile4;
-        private MetroFramework.Controls.MetroTile metroTile5;
-        private MetroFramework.Controls.MetroTile metroTile6;
-        private MetroFramework.Controls.MetroTile metroTile7;
-        private MetroFramework.Controls.MetroTile metroTile8;
-        private MetroFramework.Controls.MetroTile metroTile9;
+        private System.Windows.Forms.Panel Mpanel;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,24 +7,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using MetroFramework.Forms;
-namespace MetroDashBoard
+
+namespace WindowsFormsApplication5
 {
-    public partial class Dashboard : MetroFramework.Forms.MetroForm
+    public partial class Form1 : Form
     {
-        public Dashboard()
+        int pw;
+        bool Hided;
+
+        public Form1()
         {
             InitializeComponent();
+            pw = Mpanel.Width;
+            Hided = false;
         }
 
-        private void Dashboard_Load(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
-
         }
 
-        private void metroTile1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
+            if (Hided) button1.Text = "_\n_\n_";
+            else button1.Text = "_\n_\n_";
+            timer1.Start();
+        }
 
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (Hided) {
+                Mpanel.Width = Mpanel.Width + 20;
+                if (Mpanel.Width >= pw)
+                { 
+                    timer1.Stop();
+                    Hided = false;
+                    this.Refresh();
+                }
+            }else{
+                Mpanel.Width = Mpanel.Width -20;
+                if(Mpanel.Width <= 0)
+                {
+                    timer1.Stop();
+                    Hided = true;
+                this.Refresh();
+                }
+            }
         }
     }
 }
